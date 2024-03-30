545737dbdf043b154b50eb089a8e382d9797bf98
VaT-10 <138997593+VaT-10@users.noreply.github.com>
2024-03-30 16:49:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 API_URL = 'https://api.telegram.org/bot'
-TOKEN = '6798514474:AAFfj-qLZv7mAQTWvjq2daG8dtgQt4CDv2g'
+TOKEN = 'PASTE YOUR TOKEN HERE'
 TEXT = 'Ваше сообщение отправлено @VaTkac1'
 offset_num = -2
 line = []
@@ -19,20 +19,24 @@ while True:
                 requests.get(
                     f"{API_URL}{TOKEN}/sendMessage?chat_id={result['edited_message']['chat']['id']}&text=Нельзя редактировать сообщения!")
             else:
+                flag = False
                 chat_id = result['message']['chat']['id']
-                check = chat_id == 5873667382 and result['message']['text'] == '/check_updates'
-                try:
-                    result['message']['text']
-                    flag = True
+                is_text = result['message'].get('text')
+                check = is_text and chat_id == 5873667382 and result['message']['text'] == '/check_updates'
+                if is_text:
                     if not checking and not check:
                         line.append(result)
-                        print(line)
-                except KeyError:
-                    requests.get(f'{API_URL}{TOKEN}/sendMessage?chat_id={chat_id}&text=Можно отправлять только текст!')
-                    flag = False
+                    flag = True
+                elif result['message'].get('photo'):
+                    if not checking and not check:
+                        line.append(result)
+                    flag = True
+                else:
+                    requests.get(f'{API_URL}{TOKEN}/sendMessage?chat_id={chat_id}&text=Можно отправлять только текст и фото!')
+                    print(result)
 
                 if flag:
-                    if chat_id == 5873667382 and result['message']['text'] == '/check_updates':
+                    if is_text and check:
                         checking = True
                         requests.get(
                             f'{API_URL}{TOKEN}/sendMessage?chat_id=5873667382&text=У вас {len(line)} новых сообщений!')
@@ -48,12 +52,24 @@ while True:
                             last_name = ' ' + last_message['message']['from']['last_name']
                         else:
                             last_name = ''
-                        text = last_message['message']['text']
-                        requests.get(
-                            f'{API_URL}{TOKEN}/sendMessage?chat_id=5873667382&text={first_name}{last_name}: {text}')
+                        if last_message['message'].get('text'):
+                            text = last_message['message']['text']
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendMessage?chat_id=5873667382&text={first_name}{last_name}: {text}')
+                        elif last_message['message'].get('photo'):
+                            file_id = last_message['message']['photo'][-1]['file_id']
+                            caption = last_message['message'].get('caption', 'без подписи')
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendPhoto?chat_id=5873667382&photo={file_id}&caption={first_name}{last_name}: {caption}')
                     elif chat_id == 5873667382 and checking:
-                        requests.get(
-                            f'{API_URL}{TOKEN}/sendMessage?chat_id={checking_id}&text=@VaTkac1 ответил: {result["message"]["text"]}')
+                        if result['message'].get('text'):
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendMessage?chat_id={checking_id}&text=@VaTkac1 ответил: {result["message"]["text"]}')
+                        elif result['message'].get('photo'):
+                            file_id = result['message']['photo'][-1]['file_id']
+                            caption = result['message'].get('caption', 'без подписи')
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendPhoto?chat_id={checking_id}&photo={file_id}&caption=@VaTkac1 ответил фотографией с подписью: {caption}')
                         if not line:
                             checking = False
                             requests.get(
@@ -67,17 +83,21 @@ while True:
                             last_name = ' ' + last_message['message']['from']['last_name']
                         else:
                             last_name = ''
-                        text = last_message['message']['text']
-                        requests.get(
-                            f'{API_URL}{TOKEN}/sendMessage?chat_id=5873667382&text={first_name}{last_name}: {text}')
+                        if last_message['message'].get('text'):
+                            text = last_message['message']['text']
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendMessage?chat_id=5873667382&text={first_name}{last_name}: {text}')
+                        elif last_message['message'].get('photo'):
+                            file_id = last_message['message']['photo'][-1]['file_id']
+                            caption = last_message['message'].get('caption', 'без подписи')
+                            requests.get(
+                                f'{API_URL}{TOKEN}/sendPhoto?chat_id=5873667382&photo={file_id}&caption={first_name}{last_name}: {caption}')
 
                     if not checking:
                         requests.get(f'{API_URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
                         cat_photo = requests.get('https://api.thecatapi.com/v1/images/search')
                         if cat_photo.status_code == 200:
-                            requests.get(f'{API_URL}{TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_photo.json()[0]["url"]}')
-                            requests.get(
-                                f'{API_URL}{TOKEN}/sendMessage?chat_id={chat_id}&text=Посмотрите на котика, пока ждёте ответа :)')
+                            requests.get(f'{API_URL}{TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_photo.json()[0]["url"]}&caption=Посмотрите на котика, пока ждёте ответа :)')
                         else:
                             requests.get(
                                 f'{API_URL}{TOKEN}/sendMessage?chat_id={chat_id}&text=Тут должно было быть фото котика, но что-то пошло не так...')
